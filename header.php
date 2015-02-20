7403d625db22ccb4695105dd7437fb4306e8954e
Alex <Chavet>
2015-02-20 14:45:02 +1100

    Position:fixed bug test

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 	<?php do_action('foundationPress_layout_start'); ?>
 	
 	<nav class="tab-bar show-for-small-only">
-		<section class="left-small">
+		<section class="left-small sticky">
 			<a class="left-off-canvas-toggle " href="#"><span>MENU</span></a>
 		</section>
 		<section class="middle tab-bar-section">
